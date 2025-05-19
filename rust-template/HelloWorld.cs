c7db7aa0134c21c77c922570fc577b16b54edbac
Vladimir123123123 <shavykin.v1997@gmail.com>
2025-05-19 12:39:29 +0300

    Initialize add-rustdedicate-solution

---
+++ rust-template/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using Oxide.Core.Plugins;
+using Oxide.Core.Libraries.Covalence;
+using System;
+
+namespace Oxide.Plugins
+{
+    [Info("HelloWorld", "PublicRust", "1.0.0")]
+    [Description("Simple Hello World plugin for Rust")]
+
+    public class HelloWorld : CovalencePlugin
+    {
+        private void Init()
+        {
+            Puts("Hello, Rust World! Plugin successfully loaded.");
+        }
+
+        [ChatCommand("hello")]
+        private void HelloCommand(IPlayer player, string command, string[] args)
+        {
+            player.Reply("Hello, " + player.Name + "! Welcome to this Rust server.");
+        }
+    }
+}
